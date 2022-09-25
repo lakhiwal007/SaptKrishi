1ac9414244864f633cb20d67c958a6fdfcf6bc84
ankit lakhiwal <lakhiwalankit0002@gmail.com>
2022-09-25 13:28:58 +0000

    Update KnowMore.tsx

--- components/KnowMore.tsx
+++ components/KnowMore.tsx
@@ -7,7 +7,7 @@ type Props = {
 }
 const KnowMore = ({name}:Props) => {
 	return (
-			<div className="w-full h-1/4 hidden items-center justify-center bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-60 border border-gray-200 absolute bottom-0 group-hover:flex group-hover:transition">
+			<div className="w-full h-1/4 hidden items-center justify-center absolute bottom-5 group-hover:flex group-hover:transition">
 				<Link href={{
                     pathname: "/book",
                     query: { name: name },
