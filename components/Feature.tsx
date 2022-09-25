8ee584e2ced326fcee317a4e07823cffcd3c6fc7
ankit lakhiwal <lakhiwalankit0002@gmail.com>
2022-09-25 11:48:35 +0000

    Updated feature and products components

--- components/Feature.tsx
+++ components/Feature.tsx
@@ -1,12 +1,53 @@
 import { NextComponentType } from "next"
+import { Splide, SplideSlide } from '@splidejs/react-splide';
 import Image from "next/image"
+import '@splidejs/react-splide/css';
 
 const Feature: NextComponentType = () => {
   return (
    	<div className="w-full flex flex-col items-center justify-center mt-8">
 		<h1 data-aos="fade-up" className="text-4xl md:text-6xl">Features</h1>
-		<div data-aos="fade-up" className="w-full flex items-center justify-center relative p-4">
-			<Image src="/../public/src/features.PNG" width={600} height={400} className="object-contain"></Image>
+		<div data-aos="fade-up" className="w-full flex items-center justify-center p-4">
+			<Splide aria-label="My Favorite Images"
+            options={ {
+            rewind : true,
+            type  : 'loop',
+            perPage: 3,
+            snap   : true,
+            focus  : 'center',
+            gap   : '1rem',
+            drag   : 'free',
+            autoplay : true,
+            breakpoints: {
+                768: {
+                perPage: 2,
+                gap    : '.7rem',
+                },
+                640: {
+                perPage: 1,
+                gap    : '.7rem',
+                },
+            },
+            } }>
+            <SplideSlide className="flex items-center justify-center">
+                <Image src="/../public/src/Feature/cleanEnergy.png" width={300} height={300} className="object-fit rounded"  alt="Image 1"></Image>
+            </SplideSlide>
+            <SplideSlide className="flex items-center justify-center">
+                <Image src="/../public/src/Feature/carbonFootprint.png" width={300} height={300} className="object-fit rounded" alt="Image 2"></Image>
+            </SplideSlide>
+            <SplideSlide className="flex items-center justify-center">
+                <Image src="/../public/src/Feature/zeroMaint.png" width={300} height={300} className="object-fit rounded"  alt="Image 1"></Image>
+            </SplideSlide>
+            <SplideSlide className="flex items-center justify-center">
+                <Image src="/../public/src/Feature/chemFree.png" width={300} height={300} className="object-fit rounded" alt="Image 2"></Image>
+            </SplideSlide>
+            <SplideSlide className="flex items-center justify-center">
+                <Image src="/../public/src/Feature/saveEnergy.png" width={300} height={300} className="object-fit rounded"  alt="Image 1"></Image>
+            </SplideSlide>
+            <SplideSlide className="flex items-center justify-center">
+                <Image src="/../public/src/Feature/envFrind.png" width={300} height={300} className="object-fit rounded" alt="Image 2"></Image>
+            </SplideSlide>
+            </Splide>
 		</div>
 	</div>
   )
