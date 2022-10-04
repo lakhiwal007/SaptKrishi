385d683c31ecf52b27eed3736f40bc8720868d85
ankit lakhiwal <lakhiwalankit0002@gmail.com>
2022-10-04 17:36:55 +0000

    Update Team.tsx and team.css

--- components/Team.tsx
+++ components/Team.tsx
@@ -10,12 +10,8 @@ const Profiles: NextPage = () => {
             <a href="#">
               <div className="member-view-box">
                 <div className="member-image">
-                  <img
-                    src="https://image.flaticon.com/icons/svg/145/145867.svg"
-                    alt="Member"
-                  />
                   <div className="member-details">
-                    <h3>John Doe</h3>
+                    <h3>Founder and CEO</h3>
                   </div>
                 </div>
               </div>
@@ -25,33 +21,85 @@ const Profiles: NextPage = () => {
                 <a href="#">
                   <div className="member-view-box">
                     <div className="member-image">
-                      <img
-                        src="https://image.flaticon.com/icons/svg/145/145867.svg"
-                        alt="Member"
-                      />
+                      <img src="./src/team/nikky-jha.jpg" alt="nikky jha" />
                       <div className="member-details">
-                        <h3>Member 1</h3>
+                        <h4>Nikky Jha</h4>
                       </div>
                     </div>
                   </div>
                 </a>
-                
+              </li>
+              <li>
+                <a href="#">
+                  <div className="member-view-box">
+                    <div className="member-image">
+                      <img src="./src/team/rashmi-jha.jpg" alt="Member" />
+                      <div className="member-details">
+                        <h4>Rashmi Jha</h4>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+
+      <div className="genealogy-tree">
+        <ul>
+          <li>
+            <a href="#">
+              <div className="member-view-box">
+                <div className="member-image">
+                  <div className="member-details">
+                    <h3>Our Creative team</h3>
+                  </div>
+                </div>
+              </div>
+            </a>
+            <ul className="active">
+              <li>
+                <a href="#">
+                  <div className="member-view-box">
+                    <div className="member-image">
+                      <img src="./src/team/shivani.png" alt="Shivani Jha" />
+                      <div className="member-details">
+                        <h4>Ms. Shivani Jha</h4>
+                      </div>
+                    </div>
+                  </div>
+                </a>
               </li>
               <li>
                 <a href="#">
                   <div className="member-view-box">
                     <div className="member-image">
                       <img
-                        src="https://image.flaticon.com/icons/svg/145/145867.svg"
-                        alt="Member"
+                        src="./src/team/harsh.jpeg"
+                        alt="Mr. Harsh Marolia"
                       />
                       <div className="member-details">
-                        <h3>Member 2</h3>
+                        <h4>Mr. Harsh Marolia</h4>
                       </div>
                     </div>
                   </div>
                 </a>
-               
+              </li>
+              <li>
+                <a href="#">
+                  <div className="member-view-box">
+                    <div className="member-image">
+                      <img
+                        src="./src/team/sourabh.png"
+                        alt="Mr. Sourabh Tiwary"
+                      />
+                      <div className="member-details">
+                        <h4>Mr. Sourabh Tiwary</h4>
+                      </div>
+                    </div>
+                  </div>
+                </a>
               </li>
             </ul>
           </li>
